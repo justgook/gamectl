b150e88dc317d573b483ba0f7a81f50281098f32
Romāns Potašovs <justgook@gmail.com>
2025-11-14 00:54:37 +0200

    ➕ add new tree structure

---
+++ pkg/tree3/tree.go
@@ -0,0 +1,81 @@
+package tree
+
+import (
+	"iter"
+)
+
+type Tree []*Node
+
+type Node struct {
+	Data     map[string]string `json:"data"`
+	ParentId int               `json:"parent,omitempty"`
+}
+
+func (t *Tree) Add(parent int, data map[string]string) *Node {
+	node := &Node{Data: data, ParentId: parent}
+	*t = append(*t, node)
+
+	return node
+}
+
+func (t *Tree) Parent(node *Node) *Node {
+	parent := node.ParentId
+
+	return (*t)[parent]
+}
+
+func (t *Tree) Children(node *Node) iter.Seq[*Node] {
+	parentIdx := t.indexOf(node)
+
+	return func(yield func(*Node) bool) {
+		for _, n := range *t {
+			if n.ParentId == parentIdx {
+				if !yield(n) {
+					return
+				}
+			}
+		}
+	}
+}
+
+func (t *Tree) Traverse(root *Node) iter.Seq[*Node] {
+	return func(yield func(*Node) bool) {
+
+		stack := []*Node{root}
+
+		for len(stack) > 0 {
+			// pop
+			n := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			if !yield(n) {
+				return
+			}
+
+			// collect children
+			parentIdx := t.indexOf(n)
+			children := make([]*Node, 0)
+
+			for _, c := range *t {
+				if c.ParentId == parentIdx {
+					children = append(children, c)
+				}
+			}
+
+			// push in reverse so leftmost is visited first
+			for i := len(children) - 1; i >= 0; i-- {
+				stack = append(stack, children[i])
+			}
+		}
+	}
+}
+
+func (t *Tree) indexOf(node *Node) int {
+	for i, n := range *t {
+		if n == node {
+			return i
+		}
+	}
+
+	return -1
+}
