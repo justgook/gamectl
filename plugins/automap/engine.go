a2cacd9d8247a22bcf252ae1e66cfae095ab685b
Romāns Potašovs <justgook@gmail.com>
2025-12-15 11:13:02 +0200

    🚧(web:automap): start implementation

--- plugins/automap/engine.go
+++ plugins/automap/engine.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 
 	"github.com/justgook/gamectl/pkg/tilemap"
+	"github.com/justgook/wpm/pdk"
 )
 
 // AutomapEngine orchestrates automapping with immediate application
 type AutomapEngine struct{}
+
+// logToConsole sends a log message to the browser console
+func logToConsole(msg string) {
+	pdk.Call("host", "log", []byte(msg))
+}
 
 // Apply executes automapping: parse rules → scan & apply tile-by-tile
 func (e *AutomapEngine) Apply(
@@ -15,11 +21,20 @@ func (e *AutomapEngine) Apply(
 	inputMap *tilemap.TileMap,
 	outputMap *tilemap.TileMap,
 ) error {
+	logToConsole("[Automap] Starting automapping...")
+	logToConsole(fmt.Sprintf("[Automap] Rules map layers: %d", len(rulesMap.Layers)))
+	logToConsole(fmt.Sprintf("[Automap] Input map layers: %d", len(inputMap.Layers)))
+	logToConsole(fmt.Sprintf("[Automap] Output map layers: %d", len(outputMap.Layers)))
+
 	// 1. Parse global config
 	config, err := ParseGlobalConfig(rulesMap.Props)
 	if err != nil {
 		return fmt.Errorf("parse config: %w", err)
 	}
+
+	logToConsole(fmt.Sprintf("[Automap] Config parsed - Special tiles: Empty=%d, NonEmpty=%d, Ignore=%d, Other=%d, Negate=%d",
+		config.SpecialTiles.Empty, config.SpecialTiles.NonEmpty, config.SpecialTiles.Ignore,
+		config.SpecialTiles.Other, config.SpecialTiles.Negate))
 
 	// 2. Parse rules
 	rules, err := ParseRules(rulesMap, config)
@@ -29,6 +44,20 @@ func (e *AutomapEngine) Apply(
 
 	if len(rules) == 0 {
 		return fmt.Errorf("no rules found in rules map")
+	}
+
+	logToConsole(fmt.Sprintf("[Automap] Parsed %d rules", len(rules)))
+	for i, rule := range rules {
+		logToConsole(fmt.Sprintf("[Automap] Rule %d: %d input layers, %d output variants",
+			i, len(rule.InputLayers), len(rule.Outputs)))
+		for j, input := range rule.InputLayers {
+			logToConsole(fmt.Sprintf("[Automap]   Input %d: %d tiles, target=%s, negated=%v",
+				j, len(input.Tiles), input.TargetSelector, input.IsNegated))
+		}
+		for j, output := range rule.Outputs {
+			logToConsole(fmt.Sprintf("[Automap]   Output %d: %d tiles, target=%s, prob=%.2f",
+				j, len(output.Tiles), output.TargetSelector, output.Probability))
+		}
 	}
 
 	// 3. Initialize components
@@ -46,13 +75,20 @@ func (e *AutomapEngine) Apply(
 	width := inputMap.Layers[0].Width
 	height := inputMap.Layers[0].Height()
 
+	logToConsole(fmt.Sprintf("[Automap] Scanning input map: %dx%d tiles", width, height))
+
+	matchCount := 0
 	// Scan left-to-right, top-to-bottom
 	for y := 0; y < height; y++ {
 		for x := 0; x < width; x++ {
 			// Try each rule at this position (in order)
-			for _, rule := range rules {
+			for ruleIdx, rule := range rules {
 				output := matcher.TryMatch(rule, inputMap, x, y)
 				if output != nil {
+					matchCount++
+					if matchCount <= 10 { // Log first 10 matches
+						logToConsole(fmt.Sprintf("[Automap] Match at (%d,%d) with rule %d", x, y, ruleIdx))
+					}
 					// Apply immediately
 					err := applicator.Apply(output, outputMap, x, y)
 					if err != nil {
@@ -64,5 +100,6 @@ func (e *AutomapEngine) Apply(
 		}
 	}
 
+	logToConsole(fmt.Sprintf("[Automap] Total matches: %d", matchCount))
 	return nil
 }
