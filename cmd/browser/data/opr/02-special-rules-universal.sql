0e1dc5b9667f823a34c5eefa17c0e2b948559fc4
Romāns Potašovs <justgook@gmail.com>
2026-01-03 15:52:30 +0200

    🚧(web:unit-builder): improvim structure

--- cmd/browser/data/opr/02-special-rules-universal.sql
+++ cmd/browser/data/opr/02-special-rules-universal.sql
@@ -1,26 +1,43 @@
 -- Universal Special Rules
--- These rules are shared across all OPR game systems
+-- Rules that are shared across all OPR game systems
 
 DELETE FROM opr_special_rules WHERE category = 'universal';
 
 INSERT INTO opr_special_rules (id, name, description, category, is_stackable, universe_id) VALUES
--- Movement & Positioning
-('fearless', 'Fearless', 'Gets +1 to morale tests', 'universal', 0, NULL),
-('fast', 'Fast', 'The unit moves +2" when using Advance and Sprint actions', 'universal', 0, NULL),
-('slow', 'Slow', 'The unit moves -2" when using Advance and Sprint actions', 'universal', 0, NULL),
-('flying', 'Flying', 'May move over obstacles and other units, and ignores terrain effects', 'universal', 0, NULL),
-('strider', 'Strider', 'The unit ignores the effects of difficult terrain', 'universal', 0, NULL),
-('ambush', 'Ambush', 'This unit may be kept in reserve instead of deploying', 'universal', 0, NULL),
+-- Core Universal Rules
+('fearless', 'Fearless', 'Gets +1 to morale test rolls', 'universal', 0, NULL),
+('tough', 'Tough', 'Model has this many wounds', 'universal', 1, NULL),
+('fast', 'Fast', 'Moves +2" when using Advance and +4" when using Rush/Charge', 'universal', 0, NULL),
+('slow', 'Slow', 'Moves -2" when using Advance and -4" when using Rush/Charge', 'universal', 0, NULL),
+('flying', 'Flying', 'May move over anything, but must end activation on walkable terrain', 'universal', 0, NULL),
+('hero', 'Hero', 'May be deployed attached to a unit', 'universal', 0, NULL),
+('ambush', 'Ambush', 'May be kept off-table and deployed later', 'universal', 0, NULL),
+('scout', 'Scout', 'May move by up to 12" before the game starts', 'universal', 0, NULL),
+('stealth', 'Stealth', 'Enemies get -1 to hit when shooting at this unit', 'universal', 0, NULL),
+('strider', 'Strider', 'Ignores slow and dangerous terrain', 'universal', 0, NULL),
+('regeneration', 'Regeneration', 'When taking a wound, roll one die. On a 5+ it is ignored', 'universal', 0, NULL),
+('relentless', 'Relentless', 'May shoot and move in the same activation', 'universal', 0, NULL),
+('artillery', 'Artillery', 'May not move and shoot, but ignores cover when shooting', 'universal', 0, NULL),
+('no-retreat', 'No Retreat', 'When a unit where most models have this rule fails a morale test that causes it to be Shaken or Routed, the test counts as passed instead. Then, roll as many dice as the number of wounds it would take to fully destroy it, and for each result of 1-3 the unit takes one wound, which cant be ignored', 'universal', 0, NULL),
+('transport', 'Transport', 'May carry units inside', 'universal', 1, NULL),
+('fear', 'Fear', 'Enemies get -X to morale test rolls when in melee with this model', 'universal', 1, NULL),
+('aircra', 'Aircraft', 'Special movement and combat rules for aircraft', 'universal', 0, NULL),
+('caster', 'Caster', 'May cast X spells per activation', 'universal', 1, NULL),
+('resistance', 'Resistance', 'When a unit where all models have this rule takes wounds, roll one die for each. On a 6+ it is ignored. If the wounds were from a spell, then they are ignored on a 2+ instead', 'universal', 0, NULL),
+('infiltrate', 'Infiltrate', 'Counts as having Ambush, but may be deployed up to 1" away from enemy units', 'universal', 0, NULL),
 
--- Combat & Defense
-('tough', 'Tough', 'The unit rolls one extra defense die', 'universal', 1, NULL),
-('deadly', 'Deadly', 'Rolls of a natural 6 when attacking count as 2 hits instead of 1', 'universal', 1, NULL),
-('rending', 'Rending', 'Unmodified rolls of 6 to hit count as having AP(4)', 'universal', 0, NULL),
-('blast', 'Blast', 'Attacks get +X attacks when shooting at units with 3+ models', 'universal', 1, NULL),
-('ap', 'AP', 'Targets get -X to defense rolls', 'universal', 1, NULL),
-
--- Tactical
-('stealth', 'Stealth', 'Enemies get -1 to hit rolls when shooting at this unit from over 12" away', 'universal', 0, NULL),
-('scout', 'Scout', 'May deploy up to 12" forward after both armies have finished deploying', 'universal', 0, NULL),
-('regeneration', 'Regeneration', 'Once per activation, before taking a morale test, roll one die. On a 4+ heal one wound', 'universal', 0, NULL),
-('relentless', 'Relentless', 'May shoot even after using the Sprint action', 'universal', 0, NULL);
+-- Weapon Special Rules (normalized for junction table)
+('rending', 'Rending', 'On unmodified results of 6 to hit that arent blocked, this weapon deals 2 wounds instead of 1', 'weapon-special', 0, NULL),
+('blast', 'Blast', 'Hits all models in the target unit up to X models', 'weapon-special', 1, NULL),
+('deadly', 'Deadly', 'When rolling for wounds, AP gets +X on unmodified results of 6 to hit', 'weapon-special', 1, NULL),
+('ap', 'AP', 'Targets get -X to defense rolls', 'weapon-special', 1, NULL),
+('bane', 'Bane', 'Wounds are only ignored on unmodified rolls of 6', 'weapon-special', 0, NULL),
+('reliable', 'Reliable', 'May re-roll hit results of 1', 'weapon-special', 0, NULL),
+('indirect', 'Indirect', 'May shoot at targets that are not in line of sight', 'weapon-special', 0, NULL),
+('shred', 'Shred', 'On unmodified results of 1 to block hits, this weapon deals 1 extra wound', 'weapon-special', 0, NULL),
+('rupture', 'Rupture', 'Ignores Regeneration, and on unmodified results of 6 to hit that arent blocked, this weapon deals 1 extra wound', 'weapon-special', 0, NULL),
+('unstoppable', 'Unstoppable', 'When attacking, count enemy cover as being one step worse', 'weapon-special', 0, NULL),
+('takedown', 'Takedown', 'Ignores the Tough rule on unmodified rolls of 6', 'weapon-special', 0, NULL),
+('strafing', 'Strafing', 'Once per activation, when this model moves through enemy units, pick one of them and attack it with this weapon as if it was shooting', 'weapon-special', 0, NULL),
+('precise', 'Precise', 'Gets +1 to hit when attacking', 'weapon-special', 0, NULL),
+('furious', 'Furious', 'Gets +1 attack when charging', 'weapon-special', 0, NULL);
--- cmd/browser/data/opr/11-gf-special-rules.sql
+++ cmd/browser/data/opr/11-gf-special-rules.sql
@@ -1,24 +1,48 @@
 -- Grimdark Future - Specific Special Rules
--- Rules unique to the Grimdark Future universe
+-- Rules unique to the Grimdark Future universe, extracted from v3.5.1
 
 DELETE FROM opr_special_rules WHERE universe_id = 'grimdark-future';
 
 INSERT INTO opr_special_rules (id, name, description, category, is_stackable, universe_id) VALUES
--- Battle Brothers
-('gf-impact', 'Impact', 'Attacks get AP(+1) when charging', 'grimdark-future', 1, 'grimdark-future'),
-('gf-furious', 'Furious', 'Gets +1 attack when charging', 'grimdark-future', 0, 'grimdark-future'),
-('gf-hero', 'Hero', 'May be deployed attached to a unit', 'grimdark-future', 0, 'grimdark-future'),
+-- Alien Hives Army-Wide Rule
+('hive-bond', 'Hive Bond', 'Units where all models have this rule get +1 to morale test rolls', 'army-wide', 0, 'grimdark-future'),
 
--- Alien Hives
-('gf-poison', 'Poison', 'Attacks always wound on 4+, regardless of defense', 'grimdark-future', 0, 'grimdark-future'),
-('gf-acid-blood', 'Acid Blood', 'When killed in melee, attacker takes 1 hit', 'grimdark-future', 0, 'grimdark-future'),
-('gf-psychic', 'Psychic', 'May use psychic powers once per activation', 'grimdark-future', 1, 'grimdark-future'),
+-- Alien Hives Special Rules
+('agile', 'Agile', 'Moves +1" when using Advance, and +2" when using Rush/Charge', 'grimdark-future', 0, 'grimdark-future'),
+('breath-attack', 'Breath Attack', 'Once per activation, before attacking, roll one die. On a 2+ one enemy unit within 6" in line of sight takes 1 hit with Blast(3) and AP(1)', 'grimdark-future', 0, 'grimdark-future'),
+('caster-group', 'Caster Group', 'Pick one model with this rule in this unit to have Caster(X), where X is the total number of models with this rule in this unit. If the model is killed, pick another to be the new caster, and transfer all spell tokens to it. The caster loses all unspent spell tokens at the end of the round', 'grimdark-future', 0, 'grimdark-future'),
+('hit-and-run-fighter', 'Hit & Run Fighter', 'Once per round, units where all models have this rule may move by up to 3" after being in melee', 'grimdark-future', 0, 'grimdark-future'),
+('hive-bond-boost', 'Hive Bond Boost', 'If all models in this unit have Hive Bond, they get +2 to morale test rolls from Hive Bond (instead of only +1)', 'grimdark-future', 0, 'grimdark-future'),
+('increased-shooting-range', 'Increased Shooting Range', 'This model gets +6" range when shooting', 'grimdark-future', 0, 'grimdark-future'),
+('piercing-growth', 'Piercing Growth', 'Place one marker on this unit at the beginning of each round, starting on the round after which it deployed (counting start of game deployment as deploying on the first round). For each marker models with this rule in it get AP(+1) (up to a max. of +2). If this unit is ever Shaken, it loses all its markers', 'grimdark-future', 0, 'grimdark-future'),
+('predator-fighter', 'Predator Fighter', 'For each unmodified roll of 6 to hit in melee, this model may roll +1 attack with that weapon. This rule doesnt apply to newly generated attacks', 'grimdark-future', 0, 'grimdark-future'),
+('rapid-charge', 'Rapid Charge', 'This model moves +4" when using Charge actions', 'grimdark-future', 0, 'grimdark-future'),
+('ravage', 'Ravage', 'When its this models turn to attack in melee, roll X dice. For each 6+ the target takes one wound', 'grimdark-future', 1, 'grimdark-future'),
+('regenerative-strength', 'Regenerative Strength', 'Place one marker on this model when it ignores a wound. When in melee, pick one of its weapons to get +X attacks, where X is the number of markers on it', 'grimdark-future', 0, 'grimdark-future'),
+('self-destruct', 'Self-Destruct', 'If this model is killed in melee, the attacking unit takes X hits. If this model survives melee, after both sides have finished attacking, it is immediately killed, and the enemy unit takes X hits', 'grimdark-future', 1, 'grimdark-future'),
+('shielded', 'Shielded', 'Units where all models have this rule get +1 to defense rolls against hits that are not from spells', 'grimdark-future', 0, 'grimdark-future'),
+('spawn', 'Spawn', 'Once per game, when this model is activated, you may place a new unit of X fully within 6" of it', 'grimdark-future', 0, 'grimdark-future'),
+('spell-conduit', 'Spell Conduit', 'Casters within 12" that are from other friendly units may cast spells as if they were in this models position, and get +1 to casting rolls when doing so', 'grimdark-future', 0, 'grimdark-future'),
+('stealth-buff', 'Stealth Buff', 'Once per activation, before attacking, pick one friendly unit within 12", which gets Stealth once (next time the effect would apply)', 'grimdark-future', 0, 'grimdark-future'),
+('surprise-attack', 'Surprise Attack', 'Counts as having Infiltrate. Once deployed via this rule, roll X dice, for each 4+ one enemy unit within 3" takes 2 hits with AP(1)', 'grimdark-future', 1, 'grimdark-future'),
+('takedown-strike', 'Takedown Strike', 'Once per game, when its this models turn to attack in melee, you may pick one model in the unit as its target, and make one attack at Quality 2+ with AP(2) and Deadly(3), which is resolved as if its a unit of 1', 'grimdark-future', 0, 'grimdark-future'),
+('unpredictable', 'Unpredictable', 'When attacking, roll one die and apply one effect to all models with this rule: on a 1-3 they get AP(+1), and on a 4-6 they get +1 to hit rolls instead', 'grimdark-future', 0, 'grimdark-future'),
+('unpredictable-fighter', 'Unpredictable Fighter', 'When in melee, roll one die and apply one effect to all models with this rule: on a 1-3 they get AP(+1), and on a 4-6 they get +1 to hit rolls instead', 'grimdark-future', 0, 'grimdark-future'),
+('unpredictable-fighter-mark', 'Unpredictable Fighter Mark', 'Once per activation, before attacking, pick one enemy unit within 18", which friendly units gets Unpredictable Fighter against once (next time the effect would apply)', 'grimdark-future', 0, 'grimdark-future'),
 
--- Robot Legions
-('gf-repair', 'Repair', 'May heal wounds on nearby friendly units', 'grimdark-future', 1, 'grimdark-future'),
-('gf-destroyer', 'Destroyer', 'Ranged attacks get AP(+1)', 'grimdark-future', 0, 'grimdark-future'),
-('gf-quantum-shielding', 'Quantum Shielding', 'First wound each round is ignored', 'grimdark-future', 0, 'grimdark-future'),
+-- Aura Special Rules
+('furious-aura', 'Furious Aura', 'This model and its unit get Furious', 'grimdark-future', 0, 'grimdark-future'),
+('hive-bond-boost-aura', 'Hive Bond Boost Aura', 'This model and its unit get Hive Bond Boost', 'grimdark-future', 0, 'grimdark-future'),
+('increased-shooting-range-aura', 'Increased Shooting Range Aura', 'This model and its unit get +6" range when shooting', 'grimdark-future', 0, 'grimdark-future'),
+('rapid-charge-aura', 'Rapid Charge Aura', 'This model and its unit moves +4" when using Charge actions', 'grimdark-future', 0, 'grimdark-future'),
+('shielded-aura', 'Shielded Aura', 'This model and its unit get Shielded', 'grimdark-future', 0, 'grimdark-future'),
 
--- Universal GF Rules
-('gf-transport', 'Transport', 'May carry units inside', 'grimdark-future', 1, 'grimdark-future'),
-('gf-walker', 'Walker', 'Moves like infantry but counts as vehicle', 'grimdark-future', 0, 'grimdark-future');
+-- Other GF Rules (from other armies)
+('impact', 'Impact', 'Attacks get AP(+1) when charging', 'grimdark-future', 1, 'grimdark-future'),
+('poison', 'Poison', 'Attacks always wound on 4+, regardless of defense', 'grimdark-future', 0, 'grimdark-future'),
+('acid-blood', 'Acid Blood', 'When killed in melee, attacker takes 1 hit', 'grimdark-future', 0, 'grimdark-future'),
+('psychic', 'Psychic', 'May use psychic powers once per activation', 'grimdark-future', 1, 'grimdark-future'),
+('repair', 'Repair', 'May heal wounds on nearby friendly units', 'grimdark-future', 1, 'grimdark-future'),
+('destroyer', 'Destroyer', 'Ranged attacks get AP(+1)', 'grimdark-future', 0, 'grimdark-future'),
+('quantum-shielding', 'Quantum Shielding', 'First wound each round is ignored', 'grimdark-future', 0, 'grimdark-future'),
+('walker', 'Walker', 'Moves like infantry but counts as vehicle', 'grimdark-future', 0, 'grimdark-future');
