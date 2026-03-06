be2c4b7f1bb4f26089577a50bb06d5d438f9b80e
Romāns Potašovs <justgook@gmail.com>
2026-03-06 13:51:42 +0200

    🚧(web:game): assets load

--- plugins/game/plugin.mk
+++ plugins/game/plugin.mk
@@ -1,6 +1,7 @@
 PLUGIN_C_SOURCES := \
-  plugins/game/web/main.c \
-  plugins/game/main.c
+	plugins/game/web/main.c \
+	plugins/game/web/asset_io_web.c \
+	plugins/game/main.c
 
 PLUGIN_CFLAGS := \
   -I plugins/game/web/wasm-include
