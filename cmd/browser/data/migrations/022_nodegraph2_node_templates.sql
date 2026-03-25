c06093c42a58834cbeec795d3aece30602ad138c
Romāns Potašovs <justgook@gmail.com>
2026-03-25 22:34:46 +0200

    🚧(plugin:game2): working on tilemap

--- cmd/browser/data/migrations/022_nodegraph2_node_templates.sql
+++ cmd/browser/data/migrations/022_nodegraph2_node_templates.sql
@@ -109,4 +109,9 @@ INSERT OR IGNORE INTO nodegraph2_node_templates (name, kind, data) VALUES
         'Demo UVs',
         2,
         '{"kind":2,"name":"Demo UVs","codePath":"local:/assets/ng/nodegraph2/uvs.lua","outputs":[{"outputId":1,"name":"uvs","value":""}]}'
+    ),
+    (
+        'Respack Generate Odin',
+        2,
+        '{"kind":2,"name":"Respack Generate Odin","codePath":"local:/assets/ng/nodegraph2/respack-generate-odin.lua","inputs":[{"inputId":1,"name":"schema","defaultValue":""},{"inputId":2,"name":"packageName","defaultValue":"main"},{"inputId":3,"name":"outputFile","defaultValue":"/tmp/respack-decoder.odin"}],"outputs":[{"outputId":1,"name":"odinSource","value":""},{"outputId":2,"name":"outputFile","value":""},{"outputId":3,"name":"error","value":""}]}'
     );
