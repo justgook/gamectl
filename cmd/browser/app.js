cdabc1f1c86c1b111eb4f9ab2578df4549f7de9a
Romāns Potašovs <justgook@gmail.com>
2026-05-08 16:09:55 +0300

    🚀(browser:markov): init view

--- cmd/browser/app.js
+++ cmd/browser/app.js
@@ -18,9 +18,10 @@ let currentThemeStylesheetObjectUrl = ''
 // `
 //
 const DEFAULT_LAYOUT = `
-  <view-tilemap data-source="/edge_rules.map.json" />
-  <view-ng setup="0:v:50" />
-  <view-animation setup="0:h:50" />
+  <view-markov data-source="Basic" />
+  <view-tilemap data-source="/edge_rules.map.json" setup="0:v:50" />
+  <view-ng setup="1:h:50" />
+  <view-animation setup="1:v:50" />
 `
 
 function decodeOutput(result) {
