b0bc8ed79f5dda8804392dde3b3476b41da4dd0d
Romāns Potašovs <justgook@gmail.com>
2026-05-15 19:31:18 +0300

    🚀(plugin:treegen): add ability to build real data

--- plugins/treegen.comp/main.go
+++ plugins/treegen.comp/main.go
@@ -3,6 +3,9 @@
 package main
 
 import (
+	"sort"
+
+	gamstree "github.com/justgook/gams/sdk/go/tree"
 	treegenerator "github.com/kkgams/treegen/internal/gams/tree-generator/tree-generator"
 	"github.com/kkgams/treegen/treegen"
 	"go.bytecodealliance.org/cm"
@@ -14,19 +17,47 @@ func (rng *MyRandom) Float64() float64 {
 	return 6.66
 }
 func (rng *MyRandom) Intn(n int) int {
-	return int(n)
+	return 0
 }
+
 func init() {
-	treegenerator.Exports.Gen = func(config treegenerator.GenerateTreeConfig) (result cm.Result[treegenerator.Tree, treegenerator.Tree, string]) {
-		_, _ = treegen.GenerateTree(&MyRandom{}, &treegen.GenerateTreeConfig{
+	treegenerator.Exports.Gen = func(config treegenerator.GenerateTreeConfig) cm.Result[treegenerator.Tree, treegenerator.Tree, string] {
+		generated, err := treegen.GenerateTree(&MyRandom{}, &treegen.GenerateTreeConfig{
 			NodeCount:    int(config.NodeCount),
 			MaxDepth:     int(config.MaxDepth),
 			MaxBranching: int(config.MaxBranching),
 			RootBranches: int(config.RootBranches),
 		})
-		return result
+		if err != nil {
+			return cm.Err[cm.Result[treegenerator.Tree, treegenerator.Tree, string]](err.Error())
+		}
 
+		return cm.OK[cm.Result[treegenerator.Tree, treegenerator.Tree, string]](toWITTree(generated))
 	}
+}
+
+func toWITTree(t gamstree.Tree) treegenerator.Tree {
+	nodes := make([]treegenerator.Node, len(t))
+	for i, node := range t {
+		nodes[i] = treegenerator.Node{
+			Data:     toWITData(node.Data),
+			ParentID: int32(node.ParentId),
+		}
+	}
+	return treegenerator.Tree(cm.ToList(nodes))
+}
+
+func toWITData(data map[string]string) cm.List[treegenerator.DataEntry] {
+	entries := make([]treegenerator.DataEntry, 0, len(data))
+	keys := make([]string, 0, len(data))
+	for key := range data {
+		keys = append(keys, key)
+	}
+	sort.Strings(keys)
+	for _, key := range keys {
+		entries = append(entries, treegenerator.DataEntry{key, data[key]})
+	}
+	return cm.ToList(entries)
 }
 
 // main is required for the `wasi` target, even if it isn't used.
