d298e9aed58a11e1c9a16a50ff7b2876b0b2942f
Romāns Potašovs <justgook@gmail.com>
2026-05-25 09:18:07 +0300

    🚧(view:ng): offset map for testing

--- examples/demo/pipe/mock/tilemaps.lua
+++ examples/demo/pipe/mock/tilemaps.lua
@@ -24,7 +24,7 @@ for _, image in ipairs(images) do
 		lut_uv = { u1, v1, u2, v2 },
 	})
 
-	table.insert(position_comps, { 0,0 })
+	table.insert(position_comps, { 0,  -32*64 })
 	entityId = entityId + 1
 	table.insert(entity_ids, entityId)
 end
